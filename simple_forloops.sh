8b3184e0778165fd8ddc2f91d43e7a46b37a3f48
tomomis86 <tomomi.sunayama@gmail.com>
2014-06-20 14:07:35 -0400

    change the program

--- simple_forloops.sh
+++ simple_forloops.sh
@@ -1,8 +1,11 @@
 #This is fun :D
 COUNTER=0
 
-while [ $COUNTER -lt 10 ];
-do
-    echo The counter is at $COUNTER
-    let COUNTER=COUNTER+1
+#while [ $COUNTER -lt 10 ];
+#do
+#    echo The counter is at $COUNTER
+#    let COUNTER=COUNTER+1
+
+for i in {1..10}; do
+    echo $i
 done
